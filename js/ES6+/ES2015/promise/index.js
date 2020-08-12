6bdebe3124ad014124499cefb678252c7635be26
张弛 <624593758@qq.com>
2020-08-12 20:38:58 +0800

    添加手写promise

--- js/ES6+/ES2015/promise/index.js
+++ js/ES6+/ES2015/promise/index.js
@@ -12,38 +12,7 @@ const p3 = new Promise((resolve, reject) => {
   }, 1000)
 })
 
-// 手写all
-Promise.all_ = function(arr) {
-  return new Promise((resolve, reject) => {
-    let count = 0;
-    let results = [];
-    for(let i = 0; i < arr.length; i++){
-      Promise.resolve(arr[i])
-      .then((res) => {
-        count++;
-        results.push(res);
-        if (count === arr.length) {
-          resolve(results);
-        }
-      })
-      .catch((err) => {
-        reject(err);
-      })
-    }
-  })
-}
-// Promise.all([p1, p2, p3]).then(
-//   res => {
-//   console.log(res)
-// }, 
-//   err => {
-//     console.log('失败' + err)
-//   }
-// ).catch(error => {
-//   console.log('最后回调')
-// })
-
-Promise.all_([p1, p2, p3]).then(
+Promise.all([p1, p2, p3]).then(
   res => {
   console.log(res)
 }, 
--- js/手写方法/promise.js
+++ js/手写方法/promise.js
@@ -1,5 +1,58 @@
-// all
-Promise.all_ = function(arr) {
+class Promise_ {
+  constructor(cb){
+    this.status = 'PEDDING';
+    this.resolveCallback = [];
+    this.rejectCallback = [];
+    this.value;
+    this.error;
+    this.resolve = (value) => {
+      if (this.status !== 'PEDDING') {
+        return
+      }
+      this.status = 'FULFILLED';
+      this.value = value;
+      this.resolveCallback.forEach(cb => cb(this.value));
+    }
+    this.reject = (err) => {
+      if (this.status !== 'PEDDING') {
+        return
+      }
+      this.status = 'REJECTED';
+      this.error = err;
+      this.rejectCallback.forEach(cb => cb(this.error));
+    }
+    cb(this.resolve, this.reject);
+  }
 
+  then(cb1, cb2){
+    if (this.status === 'PEDDING') {
+      cb1 && this.resolveCallback.push(cb1);
+      cb2 && this.rejectCallback.push(cb2);
+    } else if(this.status === 'FULFILLED'){
+      cb1 && cb1(this.value)
+    } else {
+      cb2 && cb2(this.error)
+    }
+    return new Promise_((resolve) => {resolve()});
+  }
 }
-
+// 手写all
+Promise.all_ = function(arr) {
+  return new Promise((resolve, reject) => {
+    let count = 0;
+    let results = [];
+    for(let i = 0; i < arr.length; i++){
+      Promise.resolve(arr[i])
+      .then((res) => {
+        count++;
+        results.push(res);
+        if (count === arr.length) {
+          resolve(results);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      })
+    }
+  })
+}
